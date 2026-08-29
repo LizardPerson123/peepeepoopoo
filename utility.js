c5bd3cb120bc6ddb3a58f86ccd662b5fd723343b
LizardPerson123 <dylanerck@1203landmark.com>
2026-08-28 23:11:39 -0500

    Add files via upload

--- utility.js
+++ utility.js
@@ -59,10 +59,7 @@ function playSound(url) {
   arrayOfDoom.push(Audio)
   Audio.play()
 
-  alert("no")
-
   Audio.onended = function() {
-    alert("yes")
     removeItem(arrayOfDoom, Audio)
     Audio.remove()
   }
